22ca477f348a5a104b8b103ff117fb88a80a87f7
mrayson <matt.rayson@gmail.com>
2018-11-28 12:04:55 +0800

    Created a windows batch file for compilation

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
-
-set -x
-# Cannot build in our own directory
-MAKEDIR=/home/suntans/Share/code/
-cp mkdocs.yml $MAKEDIR
-cp -r docs/ $MAKEDIR
-cd $MAKEDIR/
-mkdocs build
+echo `Use the batch file instead`
+#set -x
+## Cannot build in our own directory
+#MAKEDIR=/home/suntans/Share/code/
+#cp mkdocs.yml $MAKEDIR
+#cp -r docs/ $MAKEDIR
+#cd $MAKEDIR/
+#mkdocs build
 
 #sitedir=mrayson.github.io
-sitedir=website
-mv mkdocs.yml $sitedir
-cp -r site_tmp/* $sitedir
-rm -r site_tmp
+#sitedir=website
+#mv mkdocs.yml $sitedir
+#cp -r site_tmp/* $sitedir
+#rm -r site_tmp
 #mv docs/ $sitedir
-rm -r docs/
-cd $sitedir
+#rm -r docs/
+#cd $sitedir
 
 
